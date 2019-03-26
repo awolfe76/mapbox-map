71897f7fc00d64a2af16386dbd633f66862d75a1
Andrew Wolfe <awolfe76@gmail.com>
2019-03-26 14:37:35 -0400

    loading map using hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import mapboxgl from 'mapbox-gl'
+
+const App = () => {
+  const mapDiv = useRef(null)
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiY29uZXhvbi1kZXNpZ24iLCJhIjoiY2pvdzZlb2djMXVhOTN3bmhpYzk3NndoZCJ9.On4IrAd0sgmmgd_sAqg_Gg'
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapDiv.current,
+      hash: true,
+      style: 'mapbox://styles/conexon-design/cjow6vt3sax7q2rpbj5wm7s84',
+      zoom: 10.46,
+      center: [-94.3158, 35.3941]
+    })
+
+    return function cleanUp() {
+      map.remove()
+    }
+  })
+
+  return <div id="map" ref={mapDiv} />
 }
 
-export default App;
+export default App
